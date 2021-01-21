5dc54ad3b9b515ca2f518579f5705284a7c56ec3
rrebello <roberto.rebello@gmail.com>
2021-01-20 23:19:43 -0300

    Creating Coin model

--- bag_of_holding.py
+++ bag_of_holding.py
@@ -38,6 +38,25 @@ class Item(db.Model):
     def __repr__(self):
         return "<Item {}>".format(self._id)
 
+class Coin(db.Model):
+    _id = db.Column(db.Integer, primary_key = True)
+    _name = db.Column(db.String(50), nullable = False)
+    _value = db.Column(db.Integer, nullable = False, default = 1)
+    _quantity = db.Column(db.Integer, nullable = False, default = 0)
+
+    def __init__(self, name, value, quantity, **kwargs):
+        super(Coin, self).__init__(**kwargs)
+        self._name = name
+        self._value = value
+        self._quantity = quantity
+
+    def __str__(self):
+        return "Item: {}\n\tID: {}\n\tUnit value: {}\n\tQuantity: {}\n".format(self._name, str(self._id), str(self._value),
+            str(self._quantity))
+
+    def __repr__(self):
+        return "<Coin {}>".format(self._id)
+
 @app.route("/", methods = ["POST", "GET"])
 def index():
     if flask.request.method == "POST":
