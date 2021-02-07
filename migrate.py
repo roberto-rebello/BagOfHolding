e679023aeb59cd26513e2670e07ccff789930abc
rrebello <roberto.rebello@gmail.com>
2021-02-07 12:18:24 -0300

    Creates coins on db initialization

--- migrate.py
+++ migrate.py
@@ -1,7 +1,19 @@
 from flask_migrate import Migrate
-from bag_of_holding import app, db
+from bag_of_holding import app, db, Coin
 
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     db.create_all()
+
+    copper = Coin("Copper", 1, 0)
+    silver = Coin("Silver", 10, 0)
+    gold = Coin("Gold", 100, 0)
+    platinum = Coin("Platinum", 1000, 0)
+
+    db.session.add(copper)
+    db.session.add(silver)
+    db.session.add(gold)
+    db.session.add(platinum)
+
+    db.session.commit()
