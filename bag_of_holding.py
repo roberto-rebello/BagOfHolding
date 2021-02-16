d02f269b7d14885e25f18732020cc0d731cce8e2
rrebello <roberto.rebello@gmail.com>
2021-02-16 18:42:46 -0300

    Removing calls for unused templates and reorganizing create funcion

--- bag_of_holding.py
+++ bag_of_holding.py
@@ -35,47 +35,44 @@ def logout():
     flask.session["logged_in"] = False
     return flask.redirect("/")
 
-@app.route("/", methods = ["POST", "GET"])
+@app.route("/", methods = ["GET"])
 def index():
-    if flask.request.method == "POST":
-        new_item = Item(flask.request.form["name"],
-                        flask.request.form["description"],
-                        flask.request.form["location"],
-                        flask.request.form["price"],
-                        flask.request.form["weight"],
-                        flask.request.form["quantity"],
-                        flask.request.form["type"],
-                        flask.request.form["isMagic"],
-                        flask.request.form["href"])
+    if flask.session.get("logged_in"):
+        items = Item.query.order_by(Item._name).all()
+        items_value = _getValue(items)
+        items_weight = _getWeight(items)
 
-        try:
-            db.session.add(new_item)
-            db.session.commit()
-            return flask.redirect("/")
+        coins = Coin.query.order_by(Coin._value).all()
+        party_gold = _convertDefault(coins)
 
-        except Exception as e:
-            raise
+        return flask.render_template("index.html",
+                                     items=items,
+                                     items_value=items_value,
+                                     items_weight=items_weight,
+                                     party_gold=party_gold,
+                                     coins=coins)
     else:
-        if flask.session.get("logged_in"):
-            items = Item.query.order_by(Item._name).all()
-            items_value = _getValue(items)
-            items_weight = _getWeight(items)
+        return flask.redirect("/login")
 
-            coins = Coin.query.order_by(Coin._value).all()
-            party_gold = _convertDefault(coins)
-
-            return flask.render_template("index.html",
-                                         items=items,
-                                         items_value=items_value,
-                                         items_weight=items_weight,
-                                         party_gold=party_gold,
-                                         coins=coins)
-        else:
-            return flask.redirect("/login")
-
-@app.route("/create")
+@app.route("/create", methods=["POST"])
 def create_item():
-    return flask.render_template("create.html")
+    new_item = Item(flask.request.form["name"],
+                    flask.request.form["description"],
+                    flask.request.form["location"],
+                    flask.request.form["price"],
+                    flask.request.form["weight"],
+                    flask.request.form["quantity"],
+                    flask.request.form["type"],
+                    flask.request.form["isMagic"],
+                    flask.request.form["href"])
+
+    try:
+        db.session.add(new_item)
+        db.session.commit()
+        return flask.redirect("/")
+
+    except Exception as e:
+        raise
 
 @app.route("/delete/<int:id>", methods=["POST"])
 def delete_item(id):
@@ -89,29 +86,25 @@ def delete_item(id):
     except Exception as e:
         raise
 
-@app.route("/update/<int:id>", methods=["GET", "POST"])
+@app.route("/update/<int:id>", methods=["POST"])
 def update_item(id):
     item = Item.query.get_or_404(id)
 
-    if flask.request.method == "POST":
-        item._name = flask.request.form["name"]
-        item._description = flask.request.form["description"]
-        item._location = flask.request.form["location"]
-        item._price = flask.request.form["price"]
-        item._weight = flask.request.form["weight"]
-        item._quantity = flask.request.form["quantity"]
-        item._type = flask.request.form["type"]
-        item._isMagic = flask.request.form["isMagic"]
-        item._href = flask.request.form["href"]
+    item._name = flask.request.form["name"]
+    item._description = flask.request.form["description"]
+    item._location = flask.request.form["location"]
+    item._price = flask.request.form["price"]
+    item._weight = flask.request.form["weight"]
+    item._quantity = flask.request.form["quantity"]
+    item._type = flask.request.form["type"]
+    item._isMagic = flask.request.form["isMagic"]
+    item._href = flask.request.form["href"]
 
-        try:
-            db.session.commit()
-            return flask.redirect("/")
-        except Exception as e:
-            raise
-
-    else:
-        return flask.render_template("update.html", item=item)
+    try:
+        db.session.commit()
+        return flask.redirect("/")
+    except Exception as e:
+        raise
 
 @app.route("/sell/<int:id>", methods=["POST"])
 def sell_item(id):
@@ -190,6 +183,7 @@ def update_coin():
         db.session.commit()
 
         return flask.redirect("/")
+
     except Exception as e:
         raise
 
