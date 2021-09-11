3f76518ba538b2db329d7e1683e7e8b60d95b21b
rrebello <roberto.rebello@gmail.com>
2021-09-11 19:29:56 -0300

    Updates and fixes for the journal models

--- models/journal.py
+++ models/journal.py
@@ -6,14 +6,15 @@ class Journal(db.Model):
     _author = db.Column(db.String(100), nullable = False)
     _title = db.Column(db.String(500), default = "")
     _is_private = db.Column(db.Boolean, default=False)
-
+    _user = db.Column(db.String(100), default = "")
     _creation_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    def __init__(self, author, title, is_private, **kwargs):
-        super(Item, self).__init__(**kwargs)
+    def __init__(self, author, title="", is_private=False, user="", **kwargs):
+        super(Journal, self).__init__(**kwargs)
         self._author = author
         self._title = title
         self._is_private = is_private
+        self._user = user
 
     def __str__(self):
         return "ID: {}\n\tTitle: {}\n\tAuthor: {}\n\tCreation: {}\n\tPrivate: {}\n".format(str(self._id), self._title,
@@ -29,28 +30,23 @@ class Entry(db.Model):
     _body = db.Column(db.Text, default="")
     _game_date = db.Column(db.String(50), default="")
     _location = db.Column(db.String(50), default="")
-    _image = db.Column(db.String(100), default = "")
-    _journal = db.relationship('Journal', backref=db.backref('entries', lazy=True))
-
-    _journal_id = db.Column(db.Integer, db.ForeignKey('journal.id'), nullable=False)
+    _journal = db.relationship("Journal", backref=db.backref("entries", lazy=True))
+    _journal_id = db.Column(db.Integer, db.ForeignKey("journal._id"), nullable=False)
     _creation_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-
-    def __init__(self, id, author, title, body, game_date, location, image, journal, **kwargs):
-        super(Item, self).__init__(**kwargs)
-        self._id = id
+    def __init__(self, journal, author, title="", body="", game_date="", location="", **kwargs):
+        super(Entry, self).__init__(**kwargs)
         self._author = author
         self._title = title
         self._body = body
         self._game_date = game_date
         self._location = location
-        self._image = image
         self._journal = journal
 
     def __str__(self):
-        return "ID: {}\n\tTitle: {}\n\tAuthor: {}\n\tCreation: {}\n\tBody: {}\n\tDate: {}\n\tLocation: {}\n\t \
-            Image: {}\n".format(str(self._id), self._title, self._author, str(self._creation_date), self._body,
-            self._game_date, self._location, self._image)
+        return "ID: {}\n\tTitle: {}\n\tAuthor: {}\n\tCreation: {}\n\tBody: {}\n\tDate: {}\n\tLocation: {}\n".format(
+            str(self._id), self._title, self._author, str(self._creation_date), self._body, self._game_date,
+            self._location)
 
     def __repr__(self):
         return "<Entry {}, Journal {}>".format(self._id, self._journal_id)
