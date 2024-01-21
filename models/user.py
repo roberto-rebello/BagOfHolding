6e34862edf7e33d248d94d29f8bfb3649b268999
Roberto Rebello <roberto.rebello@gmail.com>
2024-01-21 19:20:43 -0300

    Changing isAdmin field to boolean

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ class User(db.Model):
     _id = db.Column(db.Integer, primary_key = True)
     _username = db.Column(db.String(50), nullable = False)
     _password = db.Column(db.String(128), nullable = False)
-    _isAdmin  = db.Column(db.String(3), default = "No")
+    _isAdmin  = db.Column(db.Boolean, default = False)
 
     def __init__(self, username, password, is_admin = False, **kwargs):
         super(User, self).__init__(**kwargs)
