07075d40626ecb27a4f4855e8a9d70748b5d6aa3
Roberto Rebello <roberto.rebello@gmail.com>
2024-02-02 21:46:37 -0300

    Fixing dependabot alets

--- bag_of_holding.py
+++ bag_of_holding.py
@@ -13,7 +13,7 @@ from models.journal import Journal, Entry
 
 app = flask.Flask(__name__)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///db/bag.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///bag.db'
 app.secret_key = os.urandom(24)
 db.init_app(app)
 
